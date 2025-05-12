32c7a7680de14a12ff6bfdaafaf40c66e1c94306
Fırat Alçın <firatalcin04@gmail.com>
2025-05-12 23:16:50 +0300

    DbContext

---
+++ API/ECommerce/ECommerce.API/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
