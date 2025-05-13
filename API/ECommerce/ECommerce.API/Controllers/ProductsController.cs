9e054705e1b585d3bcafb490133fde7964bc6451
Fırat Alçın <firatalcin04@gmail.com>
2025-05-13 22:26:08 +0300

    APIController

---
+++ API/ECommerce/ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok("Get all products");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProducts(int id)
+        {
+            return Ok($"Get product with id {id}");
+        }
+    }
+}
