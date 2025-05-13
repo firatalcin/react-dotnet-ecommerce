39f68969b7a47a351a54de8d973955eb6f7b88a1
Fırat Alçın <firatalcin04@gmail.com>
2025-05-13 22:06:34 +0300

    Database Seed Data

--- API/ECommerce/ECommerce.API/Data/AppDbContext.cs
+++ API/ECommerce/ECommerce.API/Data/AppDbContext.cs
@@ -11,5 +11,56 @@ namespace ECommerce.API.Data
         }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(
+                 new List<Product>
+                 {
+                    new Product
+                    {
+                        Id = 1,
+                        Name = "IPhone 15",
+                        Description = "Telefon Açıklaması",
+                        Price = 70000,
+                        IsActive = true,
+                        ImageUrl = "1.jpg",
+                        Stock = 100
+                    },
+                    new Product
+                    {
+                        Id = 2,
+                        Name = "IPhone 16",
+                        Description = "Telefon Açıklaması",
+                        Price = 80000,
+                        IsActive = true,
+                        ImageUrl = "2.jpg",
+                        Stock = 100
+                    },
+                    new Product
+                    {
+                        Id = 3,
+                        Name = "IPhone 16 Pro",
+                        Description = "Telefon Açıklaması",
+                        Price = 90000,
+                        IsActive = true,
+                        ImageUrl = "3.jpg",
+                        Stock = 100
+                    },
+                    new Product
+                    {
+                        Id = 4,
+                        Name = "IPhone 16 Pro Max",
+                        Description = "Telefon Açıklaması",
+                        Price = 100000,
+                        IsActive = true,
+                        ImageUrl = "4.jpg",
+                        Stock = 100
+                    } 
+                 });
+                
+        }
     }
 }
