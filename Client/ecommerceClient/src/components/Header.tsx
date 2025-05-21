7c4bcc7039b8017f49e0a3d8156abf671f4ee10a
Fırat Alçın <firatalcin04@gmail.com>
2025-05-21 23:41:37 +0300

    Component Files

---
+++ Client/ecommerceClient/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { IProduct } from "../model/IProduct";
+
+interface Props{
+    products: IProduct[]
+}
+
+export default function Header({products}: Props){
+  return (
+    <h1>Header [{products.length}]</h1>
+  );
+}
---
+++ Client/ecommerceClient/src/components/Product.tsx
@@ -0,0 +1,13 @@
+import { IProduct } from "../model/IProduct";
+
+interface Props{
+    product: IProduct
+}
+
+export default function Product({product}: Props){
+  return(
+    <div>
+      <h3>{product.name}</h3> <p>{product.price}</p>
+    </div>
+  );
+}
